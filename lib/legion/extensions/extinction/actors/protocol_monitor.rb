b82d5c23e713a2b1a599b63df9d3f6a132c88dcb
Esity <matthewdiverson@gmail.com>
2026-03-14 22:54:02 -0500

    add protocol_monitor actor for periodic status observability
    Every 300s actor calls extinction_status for monitoring.
    No new runner method needed.

---
+++ lib/legion/extensions/extinction/actors/protocol_monitor.rb
@@ -0,0 +1,41 @@
+# frozen_string_literal: true
+
+require 'legion/extensions/actors/every'
+
+module Legion
+  module Extensions
+    module Extinction
+      module Actor
+        class ProtocolMonitor < Legion::Extensions::Actors::Every
+          def runner_class
+            Legion::Extensions::Extinction::Runners::Extinction
+          end
+
+          def runner_function
+            'extinction_status'
+          end
+
+          def time
+            300
+          end
+
+          def run_now?
+            false
+          end
+
+          def use_runner?
+            false
+          end
+
+          def check_subtask?
+            false
+          end
+
+          def generate_task?
+            false
+          end
+        end
+      end
+    end
+  end
+end
---
+++ spec/legion/extensions/extinction/actors/protocol_monitor_spec.rb
@@ -0,0 +1,45 @@
+# frozen_string_literal: true
+
+module Legion
+  module Extensions
+    module Actors
+      class Every; end # rubocop:disable Lint/EmptyClass
+    end
+  end
+end
+
+$LOADED_FEATURES << 'legion/extensions/actors/every'
+
+require_relative '../../../../../lib/legion/extensions/extinction/actors/protocol_monitor'
+
+RSpec.describe Legion::Extensions::Extinction::Actor::ProtocolMonitor do
+  subject(:actor) { described_class.new }
+
+  describe '#runner_class' do
+    it { expect(actor.runner_class).to eq Legion::Extensions::Extinction::Runners::Extinction }
+  end
+
+  describe '#runner_function' do
+    it { expect(actor.runner_function).to eq 'extinction_status' }
+  end
+
+  describe '#time' do
+    it { expect(actor.time).to eq 300 }
+  end
+
+  describe '#run_now?' do
+    it { expect(actor.run_now?).to be false }
+  end
+
+  describe '#use_runner?' do
+    it { expect(actor.use_runner?).to be false }
+  end
+
+  describe '#check_subtask?' do
+    it { expect(actor.check_subtask?).to be false }
+  end
+
+  describe '#generate_task?' do
+    it { expect(actor.generate_task?).to be false }
+  end
+end
